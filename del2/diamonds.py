fcd2e747630ade982c0e1785bd8301bde355f6eb
Hiba Khaleel <hb.khaleel@gmail.com>
2025-06-08 12:24:40 +0200

    update diamonds.py file

--- del2/diamonds.py
+++ del2/diamonds.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 # Load data
-df = pd.read_csv("./diamonds/diamonds.csv")
+df = pd.read_csv("./diamonds.csv")
 df['price_per_carat'] = df['price'] / df['carat']
 
 # Categorize features
